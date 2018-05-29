ce11de6e6816e6ad436540cb451bfdfd8c642ac7
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2018-05-29 14:03:25 +0100

    finished

--- js/index.js
+++ js/index.js
@@ -6,5 +6,29 @@ function addThree(num1,num2,num3){
   return num1 + num2 + num3;
 }
 
+function multiplyTwo(num1,num2){
+  return num1 * num2;
+}
+
+function reverseWord(str){
+  return str.split("").reverse().join("");
+}
+
+function increaseByPercentage(per,inc){
+  return per + per * (inc / 100);
+}
+
+function celciusToFahrenheit(cel){
+  return cel * 9 / 5 + 32;
+}
+
+function fahrenheitToCelcius(far){
+  return (far - 32) * 5 / 9;
+}
+
+function stripVowels(str){
+  return str.replace(/[aeiou]/ig,'');
+}
+
 // do not delete
 runTests();
